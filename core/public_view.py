0d9429f83458ffe84ca50b872128339683d9a053
deivid1801 <deividleonardo443@gmail.com>
2025-11-28 20:24:03 -0300

    Adicionando metatag Google

--- core/public_view.py
+++ core/public_view.py
@@ -1,4 +1,14 @@
-from django.shortcuts import redirect
+from django.http import HttpResponse
 
 def home(request):
-    return redirect('/api/')
+    return HttpResponse("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta name="google-site-verification" content="FAkWiYhl3uQijrO-H2lI-NzNY9VoGDrmrqdWKDGZya4" />
+        </head>
+        <body>
+            <p>API está no ar!</p>
+        </body>
+        </html>
+    """)
